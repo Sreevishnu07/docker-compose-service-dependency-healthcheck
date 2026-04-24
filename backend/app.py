c72a38fd8f5bb21f0ffe98813ba2970beae9456c
Sree Vishnu Sundaramoorthy <sreevishnu0407@gmail.com>
2026-04-24 15:43:50 +0530

    added the practical files

---
+++ backend/app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_cors import CORS
+import mysql.connector
+import os
+import time
+
+app = Flask(__name__)
+CORS(app)
+
+def connect():
+    while True:
+        try:
+            conn = mysql.connector.connect(
+                host=os.getenv("DB_HOST"),
+                user=os.getenv("DB_USER"),
+                password=os.getenv("DB_PASSWORD"),
+                database=os.getenv("DB_NAME")
+            )
+            print("Connected to DB")
+            return conn
+        except:
+            print("Waiting for DB...")
+            time.sleep(2)
+
+conn = connect()
+
+@app.route("/")
+def home():
+    return "Backend is running and DB connected!"
+
+app.run(host="0.0.0.0", port=5000)
